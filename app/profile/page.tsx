a1d7f4f40c1b98c5bc9e19deb42e4a9c7f635180
sliver232 <HealingStreams@14>
2025-03-04 12:36:52 +0100

    update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import React from 'react'
+import axios from 'axios';
 
-const page = () => {
+const page =  () => {
+  const handleLogout = async () => {
+    try{
+      await axios.post("/api/logout");
+    }catch(error){
+
+    }
+  }
   return (
     <div>
       Profile
+      <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
