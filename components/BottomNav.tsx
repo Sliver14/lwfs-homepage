edadea0e9b388586078e98f441db72c5fc916e09
sliver232 <HealingStreams@14>
2025-03-07 01:12:22 +0100

    update bottomnav

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -54,10 +54,11 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import { IoHomeOutline } from "react-icons/io5";
-import { MdLiveTv } from "react-icons/md";
-import { IoMdAppstore } from "react-icons/io";
-import { CgProfile } from "react-icons/cg";
+// import { IoHomeOutline } from "react-icons/io5";
+// import { MdLiveTv } from "react-icons/md";
+// import { IoMdAppstore } from "react-icons/io";
+// import { CgProfile } from "react-icons/cg";
+import { HomeIcon, TvIcon, ArchiveBoxIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Image from 'next/image';
 
 const BottomNav = () => {
@@ -70,41 +71,41 @@ const BottomNav = () => {
     ].includes(pathname);
     
     const Navbar = [
-        { name: "Home", route: "/home", icon: <IoHomeOutline />, dis: "translate-x-0" },
-        { name: "Livetv", route: "/livetv", icon: <MdLiveTv />, dis: "translate-x-[72px]" },
-        { name: "Store", route: "/store", icon: <IoMdAppstore />, dis: "translate-x-36" },
+        { name: "Home", route: "/home", icon: <HomeIcon className="h-5 w-5"/>, dis: "translate-x-0" },
+        { name: "Livetv", route: "/livetv", icon: <TvIcon className="h-5 w-5"/>, dis: "translate-x-[76px]" },
+        { name: "Store", route: "/store", icon: <ArchiveBoxIcon className="h-5 w-5"/>, dis: "translate-x-[152px]" },
         { 
             name: "Profile", 
             route: "/profile", 
-            icon: <CgProfile />, 
+            icon: <UserCircleIcon className="h-5 w-5"/>, 
             icon2: (
                 <div className="relative w-4 h-4">
                     <Image src="/images/profile_outline.svg" alt="Profile" fill style={{ objectFit: "cover" }} />
                 </div>
             ), 
-            dis: "translate-x-[216px]" 
+            dis: "translate-x-[228px]" 
         }
     ];
-
+    
     const activeNav = Navbar.find(menu => menu.route === pathname);
 
     if (hideNavbar) return null;
 
     return (
-        <div className='flex relative w-screen'>
+        <div className='flex relative w-screen shadow-md rounded-tr-lg'>
             {/* Bottom Navbar */}
             <div className='flex w-screen h-auto px-2 py-1 fixed shadow-2xl bg-white bottom-0 justify-center z-50 items-center rounded-t-xl lg:hidden'>
-                <ul className='flex relative gap-2'>
+                <ul className='flex relative gap-3'>
                     {/* Active Indicator */}
-                    <span className={`bg-lwfs_blue duration-500 h-16 w-16 ${activeNav ? activeNav.dis : ""} absolute rounded-full border-4 border-white -top-6`}></span>
+                    <span className={`bg-lwfs_blue duration-500 h-16 w-16 ${activeNav ? activeNav.dis : ""} absolute rounded-full border-4 border-white -top-4`}></span>
 
                     {Navbar.map((menu, i) => (
-                        <li key={i} className={`w-16 cursor-pointer py-2 duration-700 z-50 ${pathname === menu.route ? "-mt-5" : "border-none"}`}>
+                        <li key={i} className={`w-16 cursor-pointer py-2 duration-700 z-50 ${pathname === menu.route ? "-mt-3" : "border-none"}`}>
                             <Link href={menu.route} className='flex flex-col items-center text-center'>
-                                <span className={`text-xl duration-500 ${pathname === menu.route ? "text-white" : "text-black"}`}>
+                                <span className={`text-xl duration-500 ${pathname === menu.route ? "text-white" : "text-gray-800"}`}>
                                     {menu.icon}
                                 </span>
-                                <span className={`text-xs ${pathname === menu.route ? "translate-y-0.5 duration-700 text-white" : "text-black"}`}>
+                                <span className={`text-xs ${pathname === menu.route ? "translate-y-0.5 duration-700 text-white" : "text-gray-800"}`}>
                                     {menu.name}
                                 </span>
                             </Link>
