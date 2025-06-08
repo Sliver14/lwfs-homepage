b7dfb7b3bd715352396679de1892bb7c464dbe3d
sliver232 <HealingStreams@14>
2025-06-08 22:30:53 +0100

    update

--- lib/models/Cart.ts
+++ lib/models/Cart.ts
@@ -39,28 +39,3 @@ Cart.init(
 );
 
 export default Cart;
-
-
-
-
-// import { DataTypes } from "sequelize";
-// import Sequelize from "../sequelize";
-
-// const Cart = Sequelize.define("Cart", {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         autoIncrement: true,
-//         primaryKey: true,
-//       }, 
-//     userId: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//       },
-//       createdAt: {
-//         type: DataTypes.DATE,
-//         defaultValue: DataTypes.NOW,
-//       },
-     
-//     });
-    
-//   export default Cart;
--- lib/models/CartItem.ts
+++ lib/models/CartItem.ts
@@ -58,38 +58,3 @@ CartItem.init(
 );
 
 export default CartItem;
-
-
-
-// import { DataTypes } from "sequelize";
-// import Sequelize from "../sequelize";
-
-// const CartItem = Sequelize.define("CartItem", {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         autoIncrement: true,
-//         primaryKey: true,
-//       }, 
-//     cartId: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//       },
-//       productId: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//       },
-//       quantity: {
-//         type:DataTypes.INTEGER,
-//         defaultValue: 1, // Set default value
-//       },
-//       color: {
-//         type: DataTypes.STRING(255),
-//         allowNull: true,
-//       },
-//       createdAt: {
-//         type: DataTypes.DATE,
-//         defaultValue: DataTypes.NOW,
-//       },
-//     });
-    
-//   export default CartItem;
