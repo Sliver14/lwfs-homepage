79a2e6042088d3ea71afd12894c57881ea29e764
sliver232 <HealingStreams@14>
2025-06-13 19:31:12 +0100

    auto generate transactionId

--- app/api/cart/checkout/route.ts
+++ app/api/cart/checkout/route.ts
@@ -1,18 +1,28 @@
+import { nanoid } from 'nanoid';
 import { NextResponse } from "next/server";
+
 
 export async function POST(req: Request) {
     try {
       const { 
-        product_sku,
         narration,
         price,
-        merchant_wallet,
         success_url,
         fail_url,
         user_data,
        } = await req.json();
-  
+      
+       if (!narration || !price || !success_url || !fail_url) {
+        return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+      }
+
+      const transactionId = `TXN-${nanoid(10)}`;
+      console.log("Generated Transaction ID:", transactionId);
+
+
       const API_KEY = process.env.API_KEY; // Load from env variables
+      const NEXT_WALLET_ADDRESS = process.env.NEXT_WALLET_ADDRESS; 
+      
   
       // Call external API
       const response = await fetch("https://api.espees.org/v2/payment/product", {
@@ -22,10 +32,10 @@ export async function POST(req: Request) {
           "x-api-key": API_KEY as string,
         },
         body: JSON.stringify({ 
-            product_sku,
+            product_sku: transactionId,
             narration,
             price,
-            merchant_wallet,
+            merchant_wallet: NEXT_WALLET_ADDRESS,
             success_url,
             fail_url,
             user_data,
