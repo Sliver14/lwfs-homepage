e77e6cf78e9697807604099a070330250e54d2d6
sliver232 <HealingStreams@14>
2025-06-28 04:28:40 +0100

    convert to prisma updated api

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-  // experimental: {
-  //   serverActions: true
-  // }
+  /* config options here */
 };
 
 export default nextConfig;
